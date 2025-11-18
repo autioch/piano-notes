49a52dc73545d88309982711bd3b1a73922fc169
Jakub Szczepaniak <autioch@gmail.com>
2025-11-18 20:35:54 +0100

    Use official build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://autioch.github.io/',
+    base: '/piano-notes',
 	vite: {
 		plugins: [tsconfigPaths()]
 	}
